b30423db7443eda7d4c3516e77945550affdc2e4
Alex Wiese <LevelPulse@gmail.com>
2019-01-16 00:51:41 +0000

    fixed js script

--- js/vod.js
+++ js/vod.js
@@ -6,7 +6,7 @@ function getVodJson () {
         $.each( data, function (key, val) {
             items.push("<li style=\"background-image: url(http://img.youtube.com/vi/" + val + "/0.jpg);\" id='" + val + "'> <div class=videosList onclick=\"loadVideoModal( '" + val + "', '" + key + "')\">" + key + "</div> </li>");
         });
-        
+
         $("<ul>", {
             "id":"vodList",
             html: items.join("")
@@ -19,10 +19,10 @@ function loadVideoModal(vidID, vidTitle)
     var title = document.getElementById("videoLabel");
     title.innerHTML = vidTitle;
     var video = document.getElementById("video");
-    video.innerHTML = "<iframe width='560' height='315' src='http://www.youtube.com/embed/" + vidID + "' frameborder='0' allowfullscreen></iframe>";
+    video.innerHTML = "<iframe width='560' height='315' src='https://www.youtube.com/embed/" + vidID + "' frameborder='0' allowfullscreen></iframe>";
     $('#embedVideo').on('hidden.bs.modal', function ()
     {
-       video.innerHTML = ""; 
+       video.innerHTML = "";
     });
     $('#embedVideo').modal('show');
 }
