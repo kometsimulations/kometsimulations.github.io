6f6e31040e70bc6af6e763d25a9a04086c4ed252
Alex Wiese <LevelPulse@gmail.com>
2019-01-17 12:09:05 +0000

    test

--- js/streamers.js
+++ js/streamers.js
@@ -1,21 +1,21 @@
 var json;
 var test;
 
-function getTeamJson () {
+function getStreamerJson () {
     json = $.getJSON("assets/streamers.json", function (data) {
         $.each(data, function() {
             var image = this.image;
             var desc = this.description;
             var name = this.name;
 
-            var obj = document.getElementById("teamInfo");
-            obj.innerHTML = obj.innerHTML + "<div class=\"teamName\">" + name + "</div><span><div class=\"teamInfo teamPicture\" style='background-image: url(https://www.imgur.com/" + image + ".jpg);)'></div></span>" +
-                    "<div class=\"teamInfo teamDescription\">" + desc + "</div>";
+            var obj = document.getElementById("streamerInfo");
+            obj.innerHTML = obj.innerHTML + "<div class=\"streamerName\">" + name + "</div><span><div class=\"streamerInfo streamerPicture\" style='background-image: url(http://www.imgur.com/" + image + ".jpg);)'></div></span>" +
+                    "<div class=\"streamerInfo streamerDescription\">" + desc + "</div>";
         });
     });
 }
 
-function showTeam()
+function showStreamers()
 {
-    $('#team').modal('show');
+    $('#streamers').modal('show');
 }
