9e1eed7ddb801ca6d654211feb1b86c8f71d8dcb
Alex Wiese <LevelPulse@gmail.com>
2019-01-20 20:57:41 +0000

    updated js

--- js/lastupdated.js
+++ js/lastupdated.js
@@ -1,7 +1,4 @@
-<script
-  type="text/JavaScript"
-  language="JavaScript">
     onload = function() {
       document.getElementById("lastModified").innerHTML = "Last updated " + document.lastModified.split(" ")[0];
     }
-  </script>
+
